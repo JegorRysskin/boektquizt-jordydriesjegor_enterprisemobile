fe15be74d00ee734b325bcdbd66fc2375583a623
JegorRysskin <jegorrysskin@gmail.com>
2019-11-05 12:08:11 +0100

    Initial Xamarin.Forms QuizApp

---
+++ Frontend/QuizApp/QuizApp/QuizApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using QuizApp.Models;
+
+namespace QuizApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
