87635612f579110367c7e95fef84e2ee4602e312
Dries Thoelen <dries.thoelen@hotmail.com>
2019-11-26 09:44:12 +0100

    First commit

---
+++ Frontend/BoektQuiz/BoektQuiz/BoektQuiz/Models/Vraag.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BoektQuiz.Annotations;
+
+namespace BoektQuiz.Models
+{
+    public class Vraag : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        private string _answer;
+
+        public string Answer
+        {
+            get => _answer;
+            set
+            {
+                if (_answer == value) return;
+                _answer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
