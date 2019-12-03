884c8db0d7f853cdad496d3d4bbb4dcbd7672f54
JegorRysskin <jegorrysskin@gmail.com>
2019-12-03 10:25:40 +0100

    Minor namechanges

---
+++ Frontend/JuryApp/JuryApp.Core/Services/IQuizService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using JuryApp.Core.Models;
+
+namespace JuryApp.Core.Services
+{
+    public interface IQuizService
+    {
+        Task<ObservableCollection<Quiz>> GetAllQuizzes();
+    }
+}
---
+++ Frontend/JuryApp/JuryApp.Core/Services/QuizService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using JuryApp.Core.Models;
+
+namespace JuryApp.Core.Services
+{
+    public class QuizService : IQuizService
+    {
+        private HttpDataService _httpDataService;
+        public QuizService()
+        {
+            _httpDataService = new HttpDataService();
+        }
+        public async Task<ObservableCollection<Quiz>> GetAllQuizzes()
+        {
+            var result = await _httpDataService.GetAsync<ObservableCollection<Quiz>>("quiz");
+            
+            return result;
+        }
+    }
+}
