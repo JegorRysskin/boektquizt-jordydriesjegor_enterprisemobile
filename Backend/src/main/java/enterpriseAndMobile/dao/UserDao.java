114836e2289cedb7a66ba3ff867d36ac10ebca35
Jordy Verbeek <verbeek.jordy@gmail.com>
2019-12-10 15:31:17 +0100

    Added a userDao and the model for User and Role.

---
+++ Backend/src/main/java/enterpriseAndMobile/dao/UserDao.java
@@ -0,0 +1,11 @@
+package enterpriseAndMobile.dao;
+
+
+import elision.hangry.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
