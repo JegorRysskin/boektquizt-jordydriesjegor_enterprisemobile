eb29926557601cf2ece58fb4c9addb5348df7858
Jordy Verbeek <verbeek.jordy@gmail.com>
2019-12-26 16:49:10 +0100

    Added the LoginUser model to the project.

---
+++ Frontend/JuryApp/JuryApp.Core/Models/LoginUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuryApp.Core.Models
+{
+    public class LoginUser
+    {
+        public string password { get; set; }
+        public string username { get; set; }
+    }
+}
