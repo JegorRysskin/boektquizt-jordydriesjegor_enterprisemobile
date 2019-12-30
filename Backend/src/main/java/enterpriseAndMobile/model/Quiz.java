47c3c0d7ec30f182e99b0f86689b9c4142bcdcaf
JegorRysskin <jegorrysskin@gmail.com>
2019-12-30 17:55:12 +0100

    Fixed a small bug with duplication errors upon creating new quiz

--- Backend/src/main/java/enterpriseAndMobile/model/Quiz.java
+++ Backend/src/main/java/enterpriseAndMobile/model/Quiz.java
@@ -33,7 +33,7 @@ public class Quiz {
             strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
             parameters = {
                     @org.hibernate.annotations.Parameter(name = "sequence_name", value = "quiz_sequence"),
-                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "4"),
                     @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
             })
     private int id;
