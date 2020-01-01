021eb04b67829841f206fbe98c086eac4af082ee
Jordy Verbeek <verbeek.jordy@gmail.com>
2020-01-01 01:37:15 +0100

    Added a getListOfRoundsByQuizId method to the QuizService.

--- Backend/src/main/java/enterpriseAndMobile/service/QuizService.java
+++ Backend/src/main/java/enterpriseAndMobile/service/QuizService.java
@@ -4,6 +4,7 @@ import enterpriseAndMobile.dto.QuizDto;
 import enterpriseAndMobile.dto.QuizPatchDto;
 import enterpriseAndMobile.model.Quiz;
 import enterpriseAndMobile.Exception.NotFoundException;
+import enterpriseAndMobile.model.Round;
 import enterpriseAndMobile.repository.QuizRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,16 @@ public class QuizService {
     @Transactional(readOnly = true)
     public Optional<Quiz> getQuizById(int id) {
         return quizRepository.getQuizById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Round> getListOfRoundsByQuizById(int id) throws NotFoundException {
+        Optional<Quiz> quiz = quizRepository.getQuizById(id);
+        if(quiz.isPresent()){
+            return quiz.get().getRounds();
+        } else {
+            throw new NotFoundException("The quiz you tried to get wasn't found.");
+        }
     }
 
     @Transactional(readOnly = true)
