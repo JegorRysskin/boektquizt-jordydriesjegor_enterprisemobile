2e7bdac8292c00f93399430cec75a4a0f0f99e98
JegorRysskin <jegorrysskin@gmail.com>
2020-01-13 16:48:33 +0100

    Small refactor

--- Frontend/JuryApp/JuryApp/Views/ShellPage.xaml.cs
+++ Frontend/JuryApp/JuryApp/Views/ShellPage.xaml.cs
@@ -5,7 +5,6 @@ using Windows.UI.Xaml.Controls;
 
 namespace JuryApp.Views
 {
-    // TODO WTS: Change the icons and titles for all NavigationViewItems in ShellPage.xaml.
     public sealed partial class ShellPage : Page
     {
         private ShellViewModel ViewModel => ViewModelLocator.Current.ShellViewModel;
